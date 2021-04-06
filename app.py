826a2b6a92a0fafb97dc4fdb22fd500cb0f48f69
sallythetechie <sllmusanga7@gmail.com>
2021-04-06 10:34:20 +0300

    edit env

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from flask_cors import CORS
 # instantiate the app
 app = Flask(__name__)
 
-CORS(app)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
